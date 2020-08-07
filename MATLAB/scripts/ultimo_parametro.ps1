ae0c00ec4f0c84855e8c46d11f534264611c956a
Luan Haickel Araujo <luanharaujo@gmail.com>
2020-08-07 18:37:47 -0300

    Modificando scripts para que eles verifiquem se o  servidor ssh esta on
    além de verificar se o servidor esta online também resolve se o ip deve ser o local ou o global

--- MATLAB/scripts/ultimo_parametro.ps1
+++ MATLAB/scripts/ultimo_parametro.ps1
@@ -1,1 +1,22 @@
-scp -i ~\`.ssh\id_rsa bitcraze@192.168.1.135:/home/bitcraze/git/magic_juggle/dados/ultimos_parametros.csv ./dados
+$ip = "192.168.1.135"
+if(Test-NetConnection -Port 22 -InformationLevel Quiet $ip)#checando se o servidor esta local
+{
+   $online = $true
+}
+else
+{
+    $ip = "179.214.73.101"
+    if(Test-NetConnection -Port 22 -InformationLevel Quiet $ip)#cehgcando se o servidor esta online
+    {
+        $online = $true
+    }
+    else
+    {
+        $online = $false
+    }
+}
+
+if($online)#atualizando arquivos apenas se o servidor SSH estiver online
+{
+    scp -i ~\`.ssh\id_rsa bitcraze@${ip}:/home/bitcraze/git/magic_juggle/dados/ultimos_parametros.csv ./dados
+}
