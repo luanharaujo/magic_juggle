8f9cd303f9849bbacb6a9143de65e0f2f1d2738e
Luan Haickel Araujo <luanharaujo@gmail.com>
2020-08-07 17:44:51 -0300

    Alterando o código para trocar os "." por "_"
    Com isso o nome das variáveis vai ficar mais legível no matlab. Além acrescentei o comando para pular a linha do radio quando o o ponteiro do arquivo de entrada é resetado.

--- codigos/gencsv.c
+++ codigos/gencsv.c
@@ -23,11 +23,11 @@ int main(int argc, char const *argv[])
 	//a ganbiara continua no script dooutro computador
 
 
-	fp_out = fopen("/home/bitcraze/git/magic_juggle/dados/nome.txt","w");
+	fp_out = fopen("../dados/nome.txt","w");
 	fprintf(fp_out, "%s", nome);
 	fclose(fp_out);
 
-	sprintf(comando,"/home/bitcraze/git/magic_juggle/dados/%s",argv[1]); 
+	sprintf(comando,"../dados/%s",argv[1]); 
 	fp_in = fopen(comando,"r");
 	
 	strcpy(nome,comando);
@@ -37,9 +37,9 @@ int main(int argc, char const *argv[])
 	nome[tam-1] = 'v';
 
 	fp_out = fopen(nome,"w");
-	fscanf(fp_in,"Connecting to radio://0/80/2M\nConnected to radio://0/80/2M");
-	//printf("0\n");
-	do
+	fscanf(fp_in,"Connecting to radio://0/80/2M\nConnected to radio://0/80/2M");//pulando a linha que imprimi a conexão com o rário
+	
+	do//loopp para pegar e salvar os nomes das variáveis
 	{
 		fscanf(fp_in,"%c", &caractere);
 		switch (caractere)
@@ -52,6 +52,17 @@ int main(int argc, char const *argv[])
 					variavel_name[i++] = caractere;
 				}while(caractere != '\'');
 				variavel_name[--i] = '\0';
+
+				//loop para subistituir os "." por "_" para que o nome da variável fique melhor no matlab
+				i=0;
+				while(variavel_name[i] != '\0')
+				{
+					if(variavel_name[i]=='.')
+						variavel_name[i++] = '_';
+					else
+						i++;
+				}
+
 				
 				fprintf(fp_out, "%s,", variavel_name);
 			break;
@@ -61,8 +72,9 @@ int main(int argc, char const *argv[])
 			break;
 		}
 	}while(caractere != '#');
-	fseek(fp_in,0,SEEK_SET);
-	while(fscanf(fp_in,"%c", &caractere) != EOF)
+	fseek(fp_in,0,SEEK_SET);//voltando ao inicio do arquivo de entrada
+	fscanf(fp_in,"Connecting to radio://0/80/2M\nConnected to radio://0/80/2M");//pulando a linha que imprimi a conexão com o rário
+	while(fscanf(fp_in,"%c", &caractere) != EOF)//loop para pegar todos os dados
 	{
 		switch (caractere)
 		{
@@ -75,19 +87,19 @@ int main(int argc, char const *argv[])
 				}while(caractere != '\'');
 				variavel_name[--i] = '\0';
 				
-				if(strcmp(variavel_name,"tempo")==0)
+				if(strcmp(variavel_name,"tempo")==0)//tempo é inteiro
 				{
 					fscanf(fp_in,": %d", &variavel_int);
 					fprintf(fp_out, "%d,", variavel_int);
-				}else if(variavel_name[i-1] == 'Z')
+				}else if(variavel_name[i-1] == 'Z')//se terminar em Z é int_16
 				{
 					fscanf(fp_in,": %d", &variavel_int);
 					fprintf(fp_out, "%d,", variavel_int);
-				}else
+				}else//outras variáveis são float
 				{
 					fscanf(fp_in,": %f", &variavel_float);
 					fprintf(fp_out, "%f,", variavel_float);
-				}
+				}//****tem ainda que tratar para a valoar da bateria que é int também, porém eu ainda não sei qual o nome que vem no arquivo*****//
 			break;
 
 			case '#':
