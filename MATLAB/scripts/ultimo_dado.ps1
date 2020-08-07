99675c3654bafb32c3943e1c348ed310ddd12713
Luan Haickel Araujo <luanharaujo@gmail.com>
2020-08-06 22:17:54 -0300

    Comando para saber se o servidor ssh esta on-line, retorma um boleano

--- MATLAB/scripts/ultimo_dado.ps1
+++ MATLAB/scripts/ultimo_dado.ps1
@@ -1,2 +1,3 @@
 scp -i ~\`.ssh\id_rsa bitcraze@192.168.1.135:/home/bitcraze/git/magic_juggle/dados/$(ssh  -i ~\`.ssh\id_rsa bitcraze@192.168.1.135 ('ls -t /home/bitcraze/git/magic_juggle/dados | head -1')) ./dados
 scp -i ~\`.ssh\id_rsa bitcraze@192.168.1.135:/home/bitcraze/git/magic_juggle/dados/nome.txt ./dados
+#Test-NetConnection -Port 22 -InformationLevel Quiet 192.168.1.135
