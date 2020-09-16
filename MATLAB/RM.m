8a976cd1ce299a99187f6cefa8bffb7c338a92d5
Luan Haickel Araujo <luanharaujo@gmail.com>
2020-09-16 15:39:12 -0300

    fixing the rotation matrix

--- MATLAB/RM.m
+++ MATLAB/RM.m
@@ -2,8 +2,9 @@ function [RM_b2g,RM_g2b] = RM(psi,phi,theta)
     %Rotation matrixs
     RM_b2g = [  cos(psi)*cos(theta)     cos(psi)*sin(theta)*sin(phi) - sin(psi)*cos(phi)    cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi);
                 sin(psi)*cos(theta)     sin(psi)*sin(theta)*sin(phi) + cos(psi)*cos(phi)    sin(psi)*sin(theta)*cos(phi) - cos(psi)*sin(phi);
-                -sin(theta)             cos(theta)*sin(phi)                                 cos(theta)*cos(psi)                                 ];
+                -sin(theta)             cos(theta)*sin(phi)                                 cos(theta)*cos(phi)                                 ];
     
-    RM_g2b = inv(RM_b2g);
+    %RM_g2b = inv(RM_b2g);%mudar para transposta
+    RM_g2b = RM_b2g';
 end
 
